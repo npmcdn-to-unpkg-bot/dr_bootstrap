6a66f6f4f6314d734a87ac9ba3b9c716ffdd6cf1
Michael Williamson <mwilliamson@digitalriver.com>
2016-02-17 17:23:37 -0600

    worked on search page

--- search.php
+++ search.php
@@ -8,13 +8,14 @@ get_header();
 $solutions_parent    = get_field('solution_parent_page', 'option');
 $our_expertise_types = array('events', 'post', 'press_release', 'media_coverage');
 
-$areas = array(
-    $solutions_parent->ID => 'solutions',
-);
+$areas = array($solutions_parent->ID => 'solutions');
+
+
 
 $posts_array = array();
 while ( have_posts() ) {
     the_post();
+
 
     // Get the excerpt
     ob_start();
@@ -27,190 +28,194 @@ while ( have_posts() ) {
     // Categorize the results
     if (isset($areas[$tm_parent])) {
         $posts_array[$areas[$tm_parent]][] = $post;
-    } elseif (in_array($post->post_type, $our_expertise_types)) {
-        //print_r($post);
-        $posts_array['our_expertise'][] = $post;
-        //$posts_array['our_expertise']['id'] = $post->ID;
+    }else if (in_array($post->post_type, $our_expertise_types)) {
+        $posts_array[$post->post_type][] = $post;
     } else {
         $posts_array['others'][] = $post;
     }
 }
+
+if(isset($_GET['type'])){
+    $type = strtolower($_GET['type']);
+}else{
+    if(isset($_GET['post_type'])){
+        $type = strtolower($_GET['post_type']);
+    }else{
+        $type = null;
+    }
+}
+if($type == "pages" || $type == null){
+    $posts_array = $posts_array['solutions'];
+}else if($type == "resources"){
+    $posts_array = $posts_array['event'];
+}else if($type == "press_releases"){
+    $posts_array = $posts_array['press_release'];
+}else{
+    $posts_array = $posts_array['media_coverage'];
+}
+
+if($type == 'newsfeed'):
+    $label = "Newsfeed";
+elseif($type == 'press_releases'):
+    $label = "Press Releases";
+elseif($type == 'resources'):
+    $label = "Resources";
+else:
+    $label = "Pages";
+endif;
+
 ?>
+
+
+<style>
+    h1{
+        font-size:1.4em;
+        font-family: "DIN Next W01 Medium",sans-serif;
+        margin-bottom: 3px;
+    }
+
+    .type a{
+        font-size: 1.25em;
+        padding: 2px 10px;
+        font-family: "DIN Next W01 Medium",sans-serif;
+        border-bottom: solid 2px #EEE;
+        color:#646469;
+        margin:0 10px 0 0;
+    }
+
+    .type a:hover{
+        border-color:#CCC;
+        color:#646469;
+    }
+
+    .type a.selected{
+        border-color: #00A7E1;
+    }
+
+    #search{
+        width: 300px;
+        border: 1px solid #CCC;
+        padding: 10px 5px;
+        display: block;
+        float: left;
+    }
+
+    #search .button{
+        margin: 0;
+    }
+
+    .button{
+        padding: 12px 12px 7px;
+        margin: 0px;
+        display: inline-block;
+        text-decoration: none;
+        transition: all 200ms ease-out 0s;
+        font-size: 1.1em;
+        text-transform: uppercase;
+        color: #FFF;
+        background-color: #00A7E1;
+        border: 2px solid #007EB5;
+        line-height: 1;
+        cursor: pointer;
+        font-family: "DIN Next W01 Medium",sans-serif;
+    }
+
+    .type{
+        margin: 25px 0px 35px;
+    }
+
+</style>
+
+
 
     <section id="search_results">
         <div class="container">
             <?php if (have_posts()) : ?>
-                <div class="row">
-                    <div class="span_12">
-                        <div class="splitter"></div>
-                    </div>
-                </div>
-                
-                <?php if (isset($posts_array['solutions'])) : ?>
+                    <?php get_search_form(true); ?>
+
+                    <nav class="type">
+                        <a href="/?s=<?php echo $_GET['s']; ?>&amp;lang=<?php echo ICL_LANGUAGE_CODE; ?>&amp;type=pages" class="<?php echo ($type == 'pages' || $type == null) ? "selected" : null; ?>">Pages</a>
+                        <a href="/?s=<?php echo $_GET['s']; ?>&amp;lang=<?php echo ICL_LANGUAGE_CODE; ?>&amp;type=resources" class="<?php echo ($type == 'resources') ? "selected" : null; ?>">Resources</a>
+                        <a href="/?s=<?php echo $_GET['s']; ?>&amp;lang=<?php echo ICL_LANGUAGE_CODE; ?>&amp;type=press_releases" class="<?php echo ($type == 'press_releases') ? "selected" : null; ?>">Press Releases</a>
+                        <a href="/?s=<?php echo $_GET['s']; ?>&amp;lang=<?php echo ICL_LANGUAGE_CODE; ?>&amp;type=newsfeed" class="<?php echo ($type == 'newsfeed') ? "selected" : null; ?>">Newsfeed</a>
+                    </nav>
+
+                    <h1><?php echo count($posts_array)." ".$label." ".__(" results for the search, ", 'digital-river')."&#8220;".$_GET['s']; ?>&#8221;</h1>
+
+                <?php if(isset($posts_array) && count($posts_array) > 0): ?>
                 <section class="solution_results">
-                    <div class="row">
-                        <div class="span_12">
-                            <h1><?php _e('Solution Results', 'search'); ?></h1>
-                        </div>
-                    </div>
-                    
-                    <div class="row">
-                    <?php
-                    $solution_count = count($posts_array['solutions']);
-                    $i = 1;
-                    foreach ($posts_array['solutions'] as $result) : ?>
-                        <section class="col col-md-6 <?php echo ($i === 2) ? 'last':''; ?>">
-                            <div class="top dotted_splitter"></div>
-                            <section class="solution">
-                                <div class="text_container">
-                                    <header class="title"><?php echo $result->post_title; ?></header>
-                                    <p class="intro"><?php echo $result->content; ?></p>
-                                </div>
-                                <div class="button_container">
-                                    <a href="<?php echo get_permalink($result->ID); ?>" class="button">
-                                        <div class="text"><?php _e('Details', 'search'); ?></div>
-                                        <div class="arrow"></div>
-                                    </a>
-                                </div>
-                            </section>
-                            <div class="dotted_splitter"></div>
+
+                    <div class="row detailed-link-wrapper">
+                        <section class="col-md-7">
+
+                            <?php foreach ($posts_array as $result) : ?>
+                              
+                                    <?php
+                                    // switch ($result->post_type) :
+                                    //     case 'press_release':
+                                    //         $title = "Press Release";
+                                    //         break;
+                                    //     case 'post':
+                                    //         $title = "Blog Post";
+                                    //         break;
+                                    //     case 'case-stories':
+                                    //         $title = "Case Story";
+                                    //         break;
+                                    //     case 'white-papers':
+                                    //         $title = "White Paper";
+                                    //         break;
+                                    //     case 'events':
+                                    //         $title = $result->post_title;
+
+                                    //         $external = get_field( "external_link", $result->ID );
+                                    //         if($external){
+                                    //             $link = get_field( "link_url", $result->ID );
+                                    //         }
+
+                                    //         break;
+                                    // default:
+                                    //         $title = $result->post_title;
+                                    //         break;
+                                    // endswitch;
+                                    ?>
+
+                                    <section class="solution detailed-link">
+                                        <div class="text_container">
+                                            <header class="title"><?php echo $result->post_title; ?></header>
+                                            <p class="intro"><?php echo $result->content; ?></p>
+                                        </div>
+                                        <a href="<?php echo get_permalink($result->ID); ?>" class="arrow"><?php _e('Details', 'search'); ?></a>
+                                    </section>
+                            <?php endforeach; ?>
                         </section>
-                        <?php if (($i % 2) === 0) : $i = 0; ?>
-                            <div class="clear"></div>
-                            </div><div class="row">
-                        <?php endif; ?>
-                    <?php $i++; endforeach; ?>
-                    <div class="clear"></div>
-                    </div>
 
-                    <div class="row">
-                        <div class="span_12">
-                            <div class="splitter last"></div>
-                        </div>
-                    </div>
+                        <section class="col-md-5 sidebar">
+                            <?php foreach ($posts_array['solutions'] as $result) : ?>
+                                    <section class="solution">
+                                        <div class="text_container">
+                                            <header class="title"><?php echo $result->post_title; ?></header>
+                                            <p class="intro"><?php echo $result->content; ?></p>
+                                        </div>
+                                        <a href="<?php echo get_permalink($result->ID); ?>" class="arrow"><?php _e('Details', 'search'); ?></a>
+                                    </section>
+                            <?php endforeach; ?>
+                        </section>
+
+
+
                     <div class="clear"></div>
+                    </div>
                 </section>
                 <?php endif; ?>
 
-                <?php if (isset($posts_array['our_expertise'])) : ?>
-                <section class="our_expertise_results">
-                    <div class="row">
-                        <div class="span_12">
-                            <h1><?php _e('Our Expertise Results', 'search'); ?></h1>
-                        </div>
-                    </div>
-                    
-                    <div class="row">
-                        <?php
-                        $first = true;
-                        $i = 1;
-                        foreach ($posts_array['our_expertise'] as $result) :
-                            //print_r($result);
-       
-                            $link = (isset($result->fields->download_link) && $result->fields->download_link)? $result->fields->download_link: get_permalink($result->ID);
-                            
-                            // Figure out title for result
-                            switch ($result->post_type) :
-                                case 'press_release':
-                                    $title = "Press Release";
-                                    break;
-                                case 'post':
-                                    $title = "Blog Post";
-                                    break;
-                                case 'case-stories':
-                                    $title = "Case Story";
-                                    break;
-                                case 'white-papers':
-                                    $title = "White Paper";
-                                    break;
-                                case 'events':
-                                    $title = $result->post_title;
+    
 
-                                    $external = get_field( "external_link", $result->ID );
-                                    if($external){
-                                        $link = get_field( "link_url", $result->ID );
-                                    }
-
-                                    break;
-                                default:
-                                    $title = $result->post_title;
-                                    break;
-                            endswitch;
-                        ?>
-                            <div class="result_listing col col-md-6 <?php echo ($i === 2) ? 'last':''; ?>">
-                                <div class="dotted_splitter top <?php echo (!$first)? 'hidden':''; ?>"></div>
-                                <section class="container row">
-                                    <div class="image col span_2 hide_on_mobile"><img src="<?php echo get_image_path('search_icons/' . $result->post_type . '.png'); ?>"></div>
-                                    <div class="content col span_10 last">
-                                        <a href="<?php echo $link; ?>"><header class="title"><?php echo $title; ?></header></a>
-                                        <section class="body"><?php echo $result->content; ?></section>
-                                        <a href="<?php echo $link; ?>" class="link"><?php _e('Read More', 'search_results'); ?></a>
-                                    </div>
-                                    <div class="clear"></div>
-                                </section>
-                                <div class="dotted_splitter bottom"></div>
-                            </div>
-                            <?php if (($i % 2) === 0) : $i = 0; ?>
-                                <?php $first = false; ?>
-                                <div class="clear"></div>
-                                </div><div class="row">
-                            <?php endif; ?>
-                        <?php $i++; endforeach; ?>
-                        <div class="clear"></div>
-                    </div>
-
-                    <div class="row">
-                        <div class="span_12">
-                            <div class="splitter last"></div>
-                        </div>
-                    </div>
-                    <div class="clear"></div>
-                </section>
-                <?php endif; ?>
-
-                <?php if (isset($posts_array['others'])) : ?>
-                <section class="page_results">
-                    <div class="row">
-                        <div class="span_12">
-                            <h1><?php _e('Page Results', 'search'); ?></h1>
-                        </div>
-                    </div>
-                    <?php foreach ($posts_array['others'] as $result) : ?>
-                        <div class="row">
-                            <div class="span_12">
-                                <div class="dotted_splitter"></div>
-                                <section class="other_result">
-                                    <p>
-                                        <?php echo $result->content; ?>
-                                        <a href="<?php echo get_permalink($result->ID); ?>" class="link"><?php _e('Read More', 'search'); ?></a>
-                                    </p>
-                                    <div class="clear"></div>
-                                </section>
-                                <div class="clear"></div>
-                            </div>
-                            <div class="clear"></div>
-                        </div>
-                    <?php endforeach; ?>
-                    <div class="row">
-                        <div class="span_12">
-                            <div class="dotted_splitter"></div>
-                            <div class="splitter"></div>
-                        </div>
-                    </div>
-                </section>
-                <?php endif; ?>
             <div class="clear"></div>
             </div>
         <?php else : ?>
                 <section class="no_results">
                     <header>No Results</header>
                     <p class="text">Sorry, we can't seem to find what you're looking for. Please modify your search and try again.</p>
-<!--                     <div class="cta_box">
-                        <ul>
-                            <li><strong>Contact us today to discuss how we can help your business grow.</strong></li>
-                            <li><a href="http://info.digitalriver.com/contact-us" class="button"><div class="text">Contact Us</div><div class="arrow"></div></a>
-                        </ul>
-                    </div> -->
                 </section>
         <?php endif; ?>
     </section>
--- searchform.php
+++ searchform.php
@@ -1,3 +1,5 @@
 <form action="<?php echo home_url( '/' ); ?>" method="get" class="search_form">
     <input class="search_box" type="text" name="s" id="search" placeholder='' value="<?php the_search_query(); ?>">
+    <input type="hidden" name="post_type" value="<?php echo (isset($_GET['type'])) ? $_GET['type'] : null; ?>" />
+    <input type="submit" class="button" value="Search" />
 </form>
