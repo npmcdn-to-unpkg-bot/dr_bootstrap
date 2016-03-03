df0ebd562474a86d53e19db837096ccbe0f948aa
Michael Williamson <mwilliamson@digitalriver.com>
2016-03-03 09:44:59 -0600

    added updated payments page

--- functions.php
+++ functions.php
@@ -88,6 +88,12 @@ function theme_styles() {
 /**
  * Bootstrap Files
  */
+
+
+wp_register_style( 'dr_icons', get_template_directory_uri() . '/fonts/dr_icons/css/digitalriver-icons.css', array(), '3.0.1', 'all' );
+wp_enqueue_style( 'dr_icons' );
+
+
 wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/bootstrap/assets/javascripts/bootstrap.min.js', array( 'jquery' ), '3.0.1', true );
 wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/bootstrap/assets/stylesheets/_bootstrap.css', array(), '3.0.1', 'all' );
 wp_enqueue_script( 'bootstrap-js' );
