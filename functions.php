97d3e41b1be150a11df38b336191d59bf2eab668
Michael Williamson <mwilliamson@digitalriver.com>
2016-04-07 13:34:43 -0500

    bricks functionality

--- functions.php
+++ functions.php
@@ -81,28 +81,6 @@ $slides = new SlidesPostType();
 
 
 
-add_filter('acf/settings/save_json', 'my_acf_json_save_point');
-
-function my_acf_json_save_point( $path )
-{
-    $path = get_stylesheet_directory() . '/json_test';
-    
-    return $path;
-}
-
-add_filter('acf/settings/load_json', 'my_acf_json_load_point');
-
-function my_acf_json_load_point( $paths )
-{
-    unset($paths[0]);
-
-    $paths[] = get_stylesheet_directory() . '/json_test';
-    
-    return $paths;
-}
-
-
-
 /**
  * Initialize Widgets Class
  */
--- inc/setup/Initialize/ResourcesInitialize.class.php
+++ inc/setup/Initialize/ResourcesInitialize.class.php
@@ -85,4 +85,5 @@ class ResourcesInit implements Initialize
 	    }
 	    return $template_path;
 	}
+
 }
